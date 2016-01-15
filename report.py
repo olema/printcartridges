6167905c3e487a83487c8d84ba08084e69f3831e
Oleg Matushkin <matushkin.oleg@gmail.com>
2016-01-15 14:48:01 +0300

    Проверка

--- report.py
+++ report.py
@@ -6,25 +6,27 @@ def data_list(filename):
     f = open(filename, 'r')
     l = []
     for line in f:
-        temp_l = line.strip().split(',')
-        l.append([ i.strip() for i in temp_l])
+        l.append([i.strip() for i in line.strip().split(',')])
     f.close()
     return l
 
-def make_cartridge_report(data_list):
+
+def make_cartridge_report(data):
     cart_dict = dict()
-    for i in data_list:
+    for i in data:
         if i[0] in cart_dict:
             cart_dict[i[0]] += 1
         else:
             cart_dict[i[0]] = 1
     return cart_dict
 
+
 def main():
     filename = 'cartidge_replacement.txt'
     data = data_list(filename)
     cd = make_cartridge_report(data)
-    print(cd)
+    for key in sorted(cd):
+        print('{:<4} = {:>3}'.format(key, cd[key]))
 
 if __name__ == '__main__':
     main()
