b7ad0f0d6ccc98ab173edee5ce981e59d4e375f4
Oleg Matushkin <matushkin.oleg@gmail.com>
2016-01-19 14:05:16 +0300

    Добавлена функция вывода отчета по картриджам в файл

--- report.py
+++ report.py
@@ -2,7 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
+import os
+import sys
+import time
+
+# Функция создания списка из текстового файла
 def data_list(filename):
+    '''Формирует и возвращает список списков замен из текстового файла
+
+        Структура вложенного списка:
+            0. Код картриджа
+            1. Дата замены
+            2. Тип принтера
+            3. Место установки
+    '''
     f = open(filename, 'r')
     l = []
     for line in f:
@@ -10,8 +23,12 @@ def data_list(filename):
     f.close()
     return l
 
-
+# Функция создания словаря в разрезе типов картриджей и кол-ва замен
 def make_cartridge_report(data):
+    '''Формирует и возвращает словарь с кол-вом замен по типам картриджей
+
+       Структура словаря: ключ - код картриджа; значение - кол-во
+    '''
     cart_dict = dict()
     for i in data:
         if i[0] in cart_dict:
@@ -20,7 +37,16 @@ def make_cartridge_report(data):
             cart_dict[i[0]] = 1
     return cart_dict
 
+# Функция формирования словаря в разрезе места установки принтера
 def make_place_dict(data):
+    '''Формирует словарь в разрезе по местам установки принтера
+
+       Структура словаря: ключ - место установки принтера;
+                       значение - список со списками:
+                           0. Код картриджа;
+                           1. Дата замены ('дд.мм.гггг');
+                           2. Тип принтера.
+    '''
     place_dict = dict()
     for i in data:
         if i[3] in place_dict:
@@ -29,7 +55,16 @@ def make_place_dict(data):
             place_dict[i[3]] = [[i[0], i[1], i[2]]]
     return place_dict
 
+# Функция формирования словаря в разрезе типа принтера
 def make_printer_dict(data):
+    '''Формирует и возвращает словарь в разрезе типов принтеров
+
+       Структура словаря: ключ - тип принтера;
+                          значение список со списками:
+                              0. Тип картриджа;
+                              1. Дата замены;
+                              2. Место установки принтера.
+    '''
     printer_dict = {}
     for i in data:
         if i[2] in printer_dict:
@@ -38,15 +73,34 @@ def make_printer_dict(data):
             printer_dict[i[2]] = [[i[0], i[1], i[3]]]
     return printer_dict
 
+# Вывод отчета по картриджам (кол-во замен)
+def cart_out(cdict, pathtofolder):
+    ''' Функция выводит список картриджей с кол-вом замен
+
+        Выводит отчет в файл cart.report. Структура ф-ла:
+            картридж - кол-во замен
+    '''
+    f = open(pathtofolder + os.sep + 'cart.report', 'w')
+    f.write('Report time: {}\n'.format(time.strftime('%a, %d %b %Y %H:%M')))
+    f.write('---\n')
+    cdict_t = cdict.items()
+    for key in sorted(cdict_t, key=lambda key: key[1], reverse=True):
+        f.write(' {:<4} = {:>3}\n'.format(key[0], key[1]))
+    f.write('---')
+    f.close()
+
+
 def main():
+    dirname = os.path.dirname(sys.argv[0])
     filename = 'cartidge_replacement.txt'
     data = data_list(filename)
     cd = make_cartridge_report(data)
+#   Отчет список картриджей с количеством замен
     cd_t = cd.items()
+    cart_out(cd, dirname)
     for key in sorted(cd_t, key=lambda key: key[1], reverse=True):
         print('{:<4} = {:>3}'.format(key[0], key[1]))
-#    f = open('place.report', 'w')
-#    Отчет в разрезе мест установки принтера
+#   Отчет в разрезе мест установки принтера
     pr = make_place_dict(data)
     for key in sorted(pr):
         print('=' * 5)
@@ -57,9 +111,9 @@ def main():
             d += 1
 #   Отчет в разрезе типов принтеров
     d_print = make_printer_dict(data)
-    print('\n***')
+    print('\n' + '*' * 60)
     print('*** В разрезе принтеров')
-    print('***')
+    print(('*' * 60) + '\n')
     for key in sorted(d_print):
         print('=' * 5)
         print(' -> {}'.format(key))
